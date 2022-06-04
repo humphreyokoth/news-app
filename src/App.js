739b694b7112e1e55900efce8ba87f2aa52553d8
humphreyokoth <humpasp@gmail.com>
2022-06-04 14:27:22 +0300

    news pp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./Components/Card";
+import "./App.css";
 
 function App() {
+
+async function   getNews(){
+  const res = await fetch("https://github.com/cyberboysumanjay/Inshorts-News-API");
+const response = res.json();
+console.log(response);
+}
+  useEffect(()=>{
+    getNews()
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container pb-5 text-center pt-5">
+        <h1>Quick News App</h1>
+        <button className="btn " onClick={()=>getNews()}>FetchNews</button>
+      </div>
+      <div className="container">
+        <button className="btn btn-success me-2">all</button>
+        <button className="btn btn-success  me-2">all</button>
+        <button className="btn btn-success  me-2">all</button>
+        <button className="btn btn-success ">all</button>
+      </div>
+      <div className="container">
+        <Card  />
+      </div>
+
+     
     </div>
   );
 }
